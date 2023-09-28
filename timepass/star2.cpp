a94097f84e68da3c50a90c17037991d92876800c
Zuber8040 <zuberustad456@gmail.com>
2023-09-28 11:40:52 +0530

    done todays

--- timepass/star2.cpp
+++ timepass/star2.cpp
@@ -6,7 +6,14 @@ int main(){
     cin>>n;
     for(int row=0;row<n;row++){
         for(int col=0;col<n;col++){
-            
+          if(row==0 || row==n-1 ||  col==0 || col==n-1){
+            cout<<"*";
+          }
+          else cout<<" ";
         }
+        cout<<endl;
     }
 }
+
+
+
