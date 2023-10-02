5a0e7f4d843e2a30571faf5162fb842605fe9e66
Zuber8040 <zuberustad456@gmail.com>
2023-10-02 09:42:15 +0530

    Done Start

--- timepass/hollowhalfpriyamid.cpp
+++ timepass/hollowhalfpriyamid.cpp
@@ -5,14 +5,10 @@ int main(){
     cout<<"Enter the number"<<endl;
     int n;
     cin>>n;
-    for(int row=0;row<n;row++){
-        for(int col=0;col<n;col++){
-            if(col==0 ||  row==0){
-                cout<<"*";
-            }
-            else if((row==1 && col==n-1) || (row==2 && col==n-2)  || (row==2 && col==n-3)        )
-                cout<<"*";
-            else cout<<" ";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || j==0 || j==n-i-1) cout<<" * ";
+            else cout<<"  ";
         }
         cout<<endl;
     }
