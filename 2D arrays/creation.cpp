747ef99b5818553c84da73e8442f8011970733bf
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2023-10-27 11:25:18 +0530

    2D array

---
+++ 2D arrays/creation.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    //  declre in 2d
+    // int arr[3][3];
+
+    // intitizing values to 2d array
+    int brr[3][3] ={
+        {1,2,3},
+        {3,3,3},
+        {4,4,4}
+        };
+    // cout<<brr[1][2];
+
+    // tp access only rows
+    // int sz = sizeof(brr)/sizeof(int);
+
+
+
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         cout<<brr[j][i]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    
+    // to print max and minium in array
+    int arr[3][3]={
+        {5,6,9},
+        {7,1,2},
+        {4,3,12}
+    };
+    // int maxel = INT_MIN, minel = INT_MAX;
+    
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         if(maxel<arr[i][j])
+    //             maxel=arr[i][j];
+    //         if(minel>arr[i][j])
+    //             minel=arr[i][j]; 
+    //     }
+    // }
+    // cout<<"Minium ele is "<<minel<<"\n";
+    // cout<<"Maxium ele is "<<maxel;
+
+// transpose matrix
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         swap(arr[i][j],arr[j][i]);
+    //     }
+    // }
+
+    
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         cout<<arr[j][i]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    }
---
+++ 2D arrays/readme.md
@@ -0,0 +1,70 @@
+2D Array
+
+why 2D array =. if we have huge size of array then we can use 2d array 
+
+Initiaze => 
+
+
+int arr [1000][1000];
+
+int=> datatype means what type of 2d array its
+arr => name of 2d array 
+first [1000]=> no of rows
+second [1000]=> no of column 
+
+
+Access => 
+arr [index of row][index of column];
+
+how it stored in the memory => 
+
+formula => c*i+j
+            c =no .of column in arr 
+            i = ith row
+            j = jth col 
+
+
+input in 2d array
+create 2 for loops 
+cin>>arr[i][j];
+
+
+imp =>index always range from n ->n-1 && 0-> m-1     n-> no of rows / m-> no of columns
+
+why we need column bcoz it help to mapping in memory
+
+int arr[row][colnm]
+
+
+<!-- list of que -->
+
+1. find some of all arrays 
+2. find key present or not in 2d array 
+3. find min/max in 2d array
+4. tranpose matrix in 2d array
+
+
+<!-- to practice -->
+1. rotate matrix by 90
+2. Spiral print
+3. wave print /zig zag
+4. median of 2D array
+5. matrixx mutiplication
+
+
+<!-- vector form in 2D array -->
+
+
+vector<vector<int>>v;
+vector=>outter vector
+vector<int>=> inner vector
+int => data type
+v => name of vector
+
+
+
+<!-- customiaz  -->
+
+
+vector<vector<int>>arr(row size,vector<int>(4));
+here vector<int>(4)=> means size of column ;
